01a371ea31376081748ed5cd30613ab236019142
Your Name <muhdsanee87@gmail.com>
2024-04-07 14:26:19 +0100

    index movie

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./bootstrap5/css/bootstrap.css" />
+    <title>Document</title>
+  </head>
+  <body class="bg-dark d-flex align-items-center justify-content-center m-auto">
+    <div class="card p-5 bg-dark">
+      <div
+        class="card p-5 d-flex align-items-center justify-content-center bg-dark"
+      >
+        <a href="login.php" class="btn btn-lg btn-success m-2 d-block">Login</a>
+        <a href="signup.php" class="btn btn-lg btn-success m-2 d-block"
+          >Sign Up</a
+        >
+      </div>
+    </div>
+    <script src="./bootstrap5/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
